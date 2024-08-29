ae50e43bc192292797f6e7bca998a404b1ae5623
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-08-29 11:21:17 +0530

    basic cobra cli setup

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	rootCmd := &cobra.Command{
+		Use:   "searchex",
+		Short: "Search exploits",
+	}
+
+	newCmd := &cobra.Command{
+		Use:   "new",
+		Short: "Show new",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) > 0 {
+				fmt.Println("Additional argument:", args[0])
+			} else {
+				fmt.Println("No additional arguments")
+			}
+		},
+	}
+
+	rootCmd.AddCommand(newCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
